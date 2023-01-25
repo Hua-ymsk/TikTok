69f91044d290afb495cf3d6063f2a3d747811bd8
唐振宇 <2580837344@qq.com>
2023-01-25 21:13:42 +0800

    视频模块

---
+++ controller/video.go
@@ -0,0 +1,17 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type VideoAPI struct{}
+
+func (api *VideoAPI) FeedHandler(c *gin.Context) {
+
+}
+
+func (api *VideoAPI) PublishHandler(c *gin.Context) {
+
+}
+
+func (api *VideoAPI) PublishListHandler(c *gin.Context) {
+
+}
