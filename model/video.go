6e60b21e0c5ecd38f0fc2c0b5367534406da61b6
唐振宇 <2580837344@qq.com>
2023-01-25 21:14:47 +0800

    视频模块model

---
+++ model/video.go
@@ -0,0 +1,23 @@
+package model
+
+// 投稿请求
+type PublishReq struct {
+	Token string `json:"token"`
+	Data []byte `json:"data"`
+	Title string `json:"title"`
+}
+// 投稿响应
+type PunblishResp struct {
+	StausCcode uint64 `json:"staus_code"`
+	StausMsg string `json:"status_msg,optional"`
+}
+
+// 视频流请求
+type FeedReq struct {
+	LastTime int64 `json:"last_time"`
+	Token string `json:"token,optional"`
+}
+
+type Video struct {
+
+}
