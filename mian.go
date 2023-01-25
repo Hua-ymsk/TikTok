c7803884dc68014161a5b52141bb15551a6b2072
唐振宇 <2580837344@qq.com>
2023-01-25 21:15:56 +0800

    程序入口 各项初始化

---
+++ mian.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"tiktok/dao"
+	"tiktok/dao/mysql"
+	"tiktok/router"
+	"tiktok/setting"
+)
+
+func main() {
+	// 加载config
+	if err := setting.Init(); err != nil {
+		fmt.Println("load config err:", err)
+	}
+	// 原生sql
+	if err := dao.Connect(); err != nil {
+		fmt.Println(errors.New("connect error"))
+		return
+	}
+	defer dao.Close()
+	// gorm初始化
+	if err := mysql.Init(setting.Conf.MysqlConfig); err != nil {
+		fmt.Println("init mysql err:", err)
+		return
+	}
+	// router初始化
+	r := router.InitRouter()
+	if err := r.Run(fmt.Sprintf(":%d", setting.Conf.Port)); err != nil {
+		fmt.Println("init router err:", err)
+		return
+	}
+
+	// r := gin.Default()
+	// router.InitRouter()
+	// r.Run(":9090") // listen and serve on 0.0.0.0:9090
+}
