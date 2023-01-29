63939630baf9a80c9cbba93c1f761ffef57da969
唐振宇 <2580837344@qq.com>
2023-01-29 16:29:08 +0800

    video数据库操作

--- dao/mysql/video.go
+++ dao/mysql/video.go
@@ -1,1 +1,31 @@
 package mysql
+
+import (
+	"tiktok/models"
+
+	"gorm.io/gorm"
+)
+
+func SaveVideo(video *models.Video) (err error) {
+	if res := db.Save(video); res.Error != nil {
+		return res.Error
+	}
+	return
+}
+
+func ChekFollow(sender_id, user_id int64) (idfollow bool, err error) {
+	var follow models.Follow
+	// 避免回表
+	condition := "following_user_id = ? and followed_user_id = ?"
+	res := db.Select("following_user_id").Where(condition, sender_id, user_id).Take(&follow)
+	// 数据库出错
+	if res.Error != nil && res.Error != gorm.ErrRecordNotFound {
+		return false, err
+	}
+	// 未关注
+	if res.Error == gorm.ErrRecordNotFound {
+		return false, nil
+	}
+
+	return true, nil
+}
