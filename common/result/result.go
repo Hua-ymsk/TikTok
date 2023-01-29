d1755d7d3df6678e04c099489ef1cbda92106340
唐振宇 <2580837344@qq.com>
2023-01-29 16:27:48 +0800

    统一返回响应

---
+++ common/result/result.go
@@ -0,0 +1,27 @@
+package result
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	StatusCode int64  `json:"status_code"`
+	StatusMsg  string `json:"status_msg"`
+}
+
+func ResponseErr(c *gin.Context, msg string) {
+	c.JSON(http.StatusOK, Response{
+		StatusCode: -1,
+		StatusMsg: msg,
+	})
+	return
+}
+
+func ResponseSuccess(c *gin.Context) {
+	c.JSON(http.StatusOK, Response{
+		StatusCode: 0,
+		StatusMsg: "请求成功",
+	})
+}
