bed049bc140eeeb84802f87b39b58cdb53f75bce
rainyday <992864928@qq.com>
2023-01-30 02:14:41 +0800

    使用token后对赞操作进行完善

--- logic/favorite.go
+++ logic/favorite.go
@@ -2,7 +2,7 @@ package logic
 
 import (
 	"fmt"
-	"tiktok/dao"
+	"strconv"
 	"tiktok/dao/mysql"
 	"tiktok/types"
 )
@@ -10,9 +10,10 @@ import (
 // DoLike 执行赞操作
 func DoLike(token, videoId string) types.FavoriteActionResp {
 	//转换token为user_id并查询用户是否存在
-	if userId, err := dao.TokenResolution(token); err == nil {
+	if userExist, userId, _, err := mysql.QueryUserName(token); err == nil && userExist {
+		userIdStr := strconv.Itoa(int(userId))
 		//查询赞是否存在
-		exist, err := mysql.LikeExist(userId, videoId)
+		exist, err := mysql.LikeExist(userIdStr, videoId)
 		if err != nil {
 			return types.FavoriteActionResp{
 				StatusCode: 1,
@@ -21,7 +22,7 @@ func DoLike(token, videoId string) types.FavoriteActionResp {
 		}
 		//存在则取消赞，不存在则点赞
 		if exist {
-			err := mysql.DeleteLikeInfo(userId, videoId)
+			err := mysql.DeleteLikeInfo(userIdStr, videoId)
 			if err != nil {
 				return types.FavoriteActionResp{
 					StatusCode: 1,
@@ -33,7 +34,7 @@ func DoLike(token, videoId string) types.FavoriteActionResp {
 				StatusMsg:  "unlike action",
 			}
 		} else {
-			err := mysql.InsertLikeInfo(userId, videoId)
+			err := mysql.InsertLikeInfo(userIdStr, videoId)
 			if err != nil {
 				return types.FavoriteActionResp{
 					StatusCode: 1,
