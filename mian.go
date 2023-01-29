2eba79ed6a4f8c7cf7bef83133e0e0b838bde179
唐振宇 <2580837344@qq.com>
2023-01-29 16:27:14 +0800

    新增雪花算法初始化

--- mian.go
+++ mian.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"tiktok/common/snowflake"
 	"tiktok/dao"
 	"tiktok/dao/mysql"
 	"tiktok/middleware"
@@ -14,9 +15,15 @@ func main() {
 	// 加载config
 	if err := setting.Init(); err != nil {
 		fmt.Println("load config err:", err)
+		return
+	}
+	// 雪花算法初始化
+	if err := snowflake.Init(); err != nil {
+		fmt.Println("init snowflake err: ", err)
+		return
 	}
 	// logger初始化
-	if err := middleware.InitLogger(); err != nil {
+	if err := middleware.InitLogger(true); err != nil {
 		fmt.Println("init logger err:", err)
 		return
 	}
