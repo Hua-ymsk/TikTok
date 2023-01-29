6fbb4543bcf6444a5674ec95157e83eda07cdebe
唐振宇 <2580837344@qq.com>
2023-01-29 16:30:48 +0800

    follow model

---
+++ models/follows.go
@@ -0,0 +1,8 @@
+package models
+
+type Follow struct {
+	ID          int64
+	FollowingID int64  `gorm:"column:following_user_id"`
+	FollowedID  int64  `gorm:"column:followed_user_id"`
+	Relation    string `gorm:"relationship"`
+}
