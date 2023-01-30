858a86261eaafc48607f58556590a3e24d2512d3
唐振宇 <2580837344@qq.com>
2023-01-30 17:04:59 +0800

    新增feed逻辑

--- logic/video.go
+++ logic/video.go
@@ -15,12 +15,27 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/copier"
 )
 
 type VideoLogic struct{}
 
 func NewVideoLogic() *VideoLogic {
 	return &VideoLogic{}
+}
+
+func (logic *VideoLogic) Feed(latest_time int64) (list []types.Video, next_time int64, err error) {
+	videos, err := mysql.GetVideosByLatestTime(latest_time)
+	if err != nil {
+		return nil, time.Now().Unix(), err
+	}
+	if err := copier.Copy(&list, &videos); err != nil {
+		fmt.Println("copy err:", err)
+		return nil, time.Now().Unix(), err
+	}
+	next_time = videos[setting.Conf.PageSize-1].TimeStamp
+
+	return
 }
 
 func (logic *VideoLogic) SaveVideo(c *gin.Context, data *multipart.FileHeader, title string) (err error) {
@@ -44,11 +59,10 @@ func (logic *VideoLogic) SaveVideo(c *gin.Context, data *multipart.FileHeader, t
 	}
 	// 保存（200ms）
 	video := &models.Video{
-		// UserID:    c.GetInt64("user_id"),
-		UserID:    1,
-		Play_url:  dst,
-		Cover_url: cover_url,
-		Titile:    title,
+		UserID:    c.GetInt64("user_id"),
+		PlayURL:   dst,
+		CoverURL:  cover_url,
+		Title:     title,
 		TimeStamp: time.Now().Unix(),
 	}
 	if err := mysql.SaveVideo(video); err != nil {
@@ -62,7 +76,13 @@ func (logic *VideoLogic) VideoList(c *gin.Context, user_id int64) (list []types.
 	// 不该每个video中都要求author信息，数据冗杂
 	// 暂不查询author
 
-	// videos := mysql.PublishList(user_id)
-	mysql.ChekFollow(1, 1)
+	// 视频信息
+	videos, err := mysql.GetPublishList(user_id)
+	if err := copier.Copy(&list, &videos); err != nil {
+		fmt.Println("copy err:", err)
+		return nil, err
+	}
+	// 作者信息(等jack写完)
+
 	return
 }
