b8eaf48cb22b972cfd6e6f1a6039eccc08fdb17a
唐振宇 <2580837344@qq.com>
2023-01-30 15:31:08 +0800

    gorm性能优化

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -13,13 +13,16 @@ import (
 
 var db *gorm.DB
 
-func Init(cfg *setting.MysqlConfig) (err error){
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True",cfg.User,cfg.Pwd,cfg.Host,cfg.Port,cfg.DB)
+func Init(cfg *setting.MysqlConfig) (err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True", cfg.User, cfg.Pwd, cfg.Host, cfg.Port, cfg.DB)
 	db, err = gorm.Open(mysql.New(mysql.Config{
-		DSN: dsn,
-		DontSupportRenameIndex: true, // 重命名索引时采用删除并新建的方式（MySQL 5.7之后）
+		DSN:                     dsn,
+		DontSupportRenameIndex:  true, // 重命名索引时采用删除并新建的方式（MySQL 5.7之后）
 		DontSupportRenameColumn: true, // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
-	}), &gorm.Config{})
+	}), &gorm.Config{
+		SkipDefaultTransaction: true, // 禁止默认事务
+		PrepareStmt:            true, // 开启全局缓存预编译语句
+	})
 	if err != nil {
 		panic(fmt.Errorf("connect to mysql err:%+v", err))
 	}
