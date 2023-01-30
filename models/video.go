9c106747bd332eeabfce8a2daa3410e83a5d15e0
唐振宇 <2580837344@qq.com>
2023-01-30 17:05:14 +0800

    video model 修改

--- models/video.go
+++ models/video.go
@@ -1,10 +1,12 @@
 package models
 
 type Video struct {
-	ID        int64
-	UserID    int64  `gorm:"column:user_id"`
-	Play_url  string `gorm:"column:video_url"`
-	Cover_url string `gorm:"column:cover_url"`
-	Titile    string `gorm:"column:title"`
-	TimeStamp int64  `gorm:"column:timestamp"`
+	CommentCount  int64  `gorm:"column:comments_num"`
+	CoverURL      string `gorm:"column:cover_url"`
+	FavoriteCount int64  `gorm:"column:likes_num"`
+	ID            int64  `gorm:"column:id"`
+	PlayURL       string `gorm:"column:video_url"`
+	Title         string `gorm:"column:title"`
+	TimeStamp     int64  `gorm:"column:timestamp"`
+	UserID        int64  `gorm:"column:user_id"`
 }
