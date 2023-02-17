1b8dc174ab0a757f8ca2c740fe1a8db18fb2e2fc
rainyday <992864928@qq.com>
2023-02-17 23:16:00 +0800

    增加pre_msg_time字段

--- controller/message.go
+++ controller/message.go
@@ -37,6 +37,7 @@ func (api *MessageAPI) MessageChat(c *gin.Context) {
 				query:
 					token<string>:用户鉴权token
 					to_user_id<string>:对方用户id
+					pre_msg_time<int>:上次最新消息的时间
 				response:
 					 status_code<int64>:状态码，0-成功，其他值-失败
 			    	 status_msg<string|null>:返回状态描述
@@ -44,11 +45,14 @@ func (api *MessageAPI) MessageChat(c *gin.Context) {
 						id<int>:消息id
 		            	content<string>:消息内容
 		            	create_time<string>:消息发送时间 yyyy-MM-dd HH:MM:ss
+						to_user_id<int>:该消息接收者的id
+						from_user_id<int>:该消息发送者的id
 
 	*/
 	//使用中间件将token转化成user_id
 	userId := c.GetInt64("user_id")
 	toUserId := c.Query("to_user_id")
-	response := logic.DoMessageChat(userId, toUserId)
+	preMsgTime := c.Query("pre_msg_time")
+	response := logic.DoMessageChat(userId, toUserId, preMsgTime)
 	c.JSON(http.StatusOK, response)
 }
--- logic/message.go
+++ logic/message.go
@@ -2,8 +2,10 @@ package logic
 
 import (
 	"fmt"
+	"strconv"
 	"tiktok/dao/mysql"
 	"tiktok/types"
+	"time"
 )
 
 // SendMessageAction 执行发送消息
@@ -23,7 +25,15 @@ func SendMessageAction(userId int64, toUserId, content string) types.MessageActi
 }
 
 // DoMessageChat 执行查看聊天记录
-func DoMessageChat(userId int64, toUserId string) types.MessageChatResp {
+func DoMessageChat(userId int64, toUserId, preMsgTime string) types.MessageChatResp {
+	preMsgTimeInt, errCov := strconv.Atoi(preMsgTime)
+	if errCov != nil {
+		return types.MessageChatResp{
+			MessageList: nil,
+			StatusCode:  "1",
+			StatusMsg:   fmt.Sprintf("string to int error:%v", errCov),
+		}
+	}
 	//查看聊天记录
 	chats, err := mysql.SelectMessageChat(userId, toUserId)
 	if err != nil {
@@ -35,17 +45,19 @@ func DoMessageChat(userId int64, toUserId string) types.MessageChatResp {
 	}
 	var res = make([]types.Message, 0, 100)
 	for _, chat := range chats {
-		//消息发送时间 yyyy-MM-dd HH:MM:ss
-		//messageTime := time.Unix(chat.Timestamp, 0)
-		//messageTimeStr := messageTime.Format("2006-01-02 15:04:05")
-		temp := types.Message{
-			Content:    chat.Content,
-			CreateTime: chat.Timestamp,
-			//ID:         chat.ID,
-			ToUserId:   chat.ReceiveUserId,
-			FormUserId: chat.SendUserId,
+		if chat.Timestamp > int64(preMsgTimeInt) {
+			//消息发送时间 yyyy-MM-dd HH:MM:ss
+			messageTime := time.Unix(chat.Timestamp, 0)
+			messageTimeStr := messageTime.Format("2006-01-02 15:04:05")
+			temp := types.Message{
+				Content:    chat.Content,
+				CreateTime: messageTimeStr,
+				ID:         chat.ID,
+				ToUserId:   chat.ReceiveUserId,
+				FormUserId: chat.SendUserId,
+			}
+			res = append(res, temp)
 		}
-		res = append(res, temp)
 	}
 	return types.MessageChatResp{
 		MessageList: res,
